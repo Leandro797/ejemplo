714a488f691e541553f6ecb03aa98d350ebdd055
Leandro Higa <lah797@gmail.com>
2020-04-12 03:36:05 -0300

    agregamos el ascii.js

---
+++ ascii.js
@@ -0,0 +1,5 @@
+const [, , valor, posicion] = process.argv;
+
+//Se deberá ingresar en primer instancia el valor a pasar ASCII seguido de la posición del string.
+
+console.log(valor.charCodeAt(posicion));
